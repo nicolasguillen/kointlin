4ccff7d38dcde9444cf81cea3cc354b5fec8759f
nicolasguillen10 <nicolasguillen10@gmail.com>
2018-03-18 22:13:21 +0100

    Create extension file to add functionality to Observables

---
+++ app/src/main/java/com/nicolasguillen/kointlin/libs/util/ObservableExtensions.kt
@@ -0,0 +1,8 @@
+package com.nicolasguillen.kointlin.libs.util
+
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+fun Disposable.addTo(compositeDisposable: CompositeDisposable) {
+    compositeDisposable.add(this)
+}
