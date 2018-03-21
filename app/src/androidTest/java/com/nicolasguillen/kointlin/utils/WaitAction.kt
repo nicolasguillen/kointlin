7f250da24fe23e332043edaa7608ad747481919e
nicolasguillen10 <nicolasguillen10@gmail.com>
2018-03-21 17:50:25 +0100

    Espresso action that waits for the view

---
+++ app/src/androidTest/java/com/nicolasguillen/kointlin/utils/WaitAction.kt
@@ -0,0 +1,48 @@
+package com.nicolasguillen.kointlin.utils
+
+import android.support.test.espresso.PerformException
+import android.support.test.espresso.UiController
+import android.support.test.espresso.ViewAction
+import android.support.test.espresso.matcher.ViewMatchers.isRoot
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.util.HumanReadables
+import android.support.test.espresso.util.TreeIterables
+import android.view.View
+import java.util.concurrent.TimeoutException
+
+fun waitId(viewId: Int, millis: Long): ViewAction {
+    return object : ViewAction {
+        override fun getConstraints(): org.hamcrest.Matcher<View> {
+            return isRoot()
+        }
+
+        override fun getDescription(): String {
+            return "wait for a specific view with id <$viewId> during $millis millis."
+        }
+
+        override fun perform(uiController: UiController, view: View) {
+            uiController.loopMainThreadUntilIdle()
+            val startTime = System.currentTimeMillis()
+            val endTime = startTime + millis
+            val viewMatcher = withId(viewId)
+
+            do {
+                for (child in TreeIterables.breadthFirstViewTraversal(view)) {
+                    // found view with required ID
+                    if (viewMatcher.matches(child)) {
+                        return
+                    }
+                }
+
+                uiController.loopMainThreadForAtLeast(50)
+            } while (System.currentTimeMillis() < endTime)
+
+            // timeout happens
+            throw PerformException.Builder()
+                    .withActionDescription(this.description)
+                    .withViewDescription(HumanReadables.describe(view))
+                    .withCause(TimeoutException())
+                    .build()
+        }
+    }
+}
