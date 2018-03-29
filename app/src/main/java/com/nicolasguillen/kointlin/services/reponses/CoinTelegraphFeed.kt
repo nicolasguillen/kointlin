0f065af8d21ae48d94cfbcdbaab1b7139a67f09d
nicolasguillen10 <nicolasguillen10@gmail.com>
2018-03-29 23:35:12 +0200

    Remove simpleXML library

--- app/src/main/java/com/nicolasguillen/kointlin/services/reponses/CoinTelegraphFeed.kt
+++ app/src/main/java/com/nicolasguillen/kointlin/services/reponses/CoinTelegraphFeed.kt
@@ -2,37 +2,32 @@
 
 package com.nicolasguillen.kointlin.services.reponses
 
-import org.simpleframework.xml.Attribute
-import org.simpleframework.xml.Element
-import org.simpleframework.xml.ElementList
-import org.simpleframework.xml.Root
-
-@Root(name = "rss", strict = false)
+//@Root(name = "rss", strict = false)
 data class CoinTelegraphFeed @JvmOverloads constructor(
-        @field:Attribute
+//        @field:Attribute
         var version: String? = null,
-        @field:Element(name = "channel")
+//        @field:Element(name = "channel")
         var channel: CoinTelegraphChannel? = null
 )
 
-@Root(name = "channel", strict = false)
+//@Root(name = "channel", strict = false)
 data class CoinTelegraphChannel @JvmOverloads constructor(
-        @field:ElementList(inline = true, required=false)
+//        @field:ElementList(inline = true, required=false)
         var feedItems: List<CoinTelegraphItem>? = null
 )
 
-@Root(name = "item", strict = false)
+//@Root(name = "item", strict = false)
 data class CoinTelegraphItem @JvmOverloads constructor(
-        @field:Element(name="title", required = false)
+//        @field:Element(name="title", required = false)
         var title: String? = null,
-        @field:Element(name="link", required = false)
+//        @field:Element(name="link", required = false)
         var link: String? = null,
-        @field:Element(name="content", required = false)
+//        @field:Element(name="content", required = false)
         var content: CoinTelegraphMedia? = null
 )
 
-@Root(name = "content", strict = false)
+//@Root(name = "content", strict = false)
 data class CoinTelegraphMedia @JvmOverloads constructor(
-        @field:Attribute
+//        @field:Attribute
         var url: String? = null
 )
